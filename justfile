72da7ae95e51c541bd709a303aa7a4a25b75674e
Easton Potokar <contagon6@gmail.com>
2025-11-18 10:35:36 -0500

    Copy tests over from gtsam

--- justfile
+++ justfile
@@ -1,4 +1,4 @@
 pybuild:
     touch pyproject.toml
     uv --verbose sync --all-extras
-    cp -r .venv/lib/python3.12/site-packages/gtsam/_core python/gtsam
+    cp -r .venv/lib/python3.12/site-packages/gtsam/_core* python/gtsam
--- python/gtsam/__init__.py
+++ python/gtsam/__init__.py
@@ -1,1 +1,15 @@
 from gtsam._core import *  # noqa: F403
+import numpy as np
+
+
+def Point2(x: float = np.nan, y: float = np.nan):
+    """Shim for the deleted Point2 type."""
+    return np.array([x, y], dtype=float)
+
+
+global Point3  # export function
+
+
+def Point3(x: float = np.nan, y: float = np.nan, z: float = np.nan):
+    """Shim for the deleted Point3 type."""
+    return np.array([x, y, z], dtype=float)
